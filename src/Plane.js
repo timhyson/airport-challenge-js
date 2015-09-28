0d3380adf7e1d15564d310b4f8c4ba6e7a49050c
Tim Hyson <tjhyson@gmail.com>
2015-09-28 18:19:08 +0100

    plane function added

---
+++ src/Plane.js
@@ -0,0 +1,32 @@
+function Plane() {
+  this.isFlying = true;
+};
+
+Plane.prototype.land = function() {
+  this.isFlying = false;
+};
+
+Plane.prototype.takeoff = function() {
+  this.isFlying = true;
+};
+
+// }
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
